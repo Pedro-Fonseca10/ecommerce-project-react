11c33b0b9ec9d09f81b5b055066771f3a8dfaba1
Pedro <phdffonseca@gmaill.com>
2025-10-31 15:18:22 -0300

    Create mutiple routes usign react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
 import { HomePage } from "./pages/HomePage";
+
 function App() {
   return (
-    <>
-    <HomePage />
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/orders" element={<div>Orders page coming soon</div>} />
+      <Route path="/checkout" element={<div>Test Checkout Page</div>} />
+    </Routes>
   );
 }
 
